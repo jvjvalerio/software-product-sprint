7c2b62bd087b59929d78816b948349f47f562e78
Julio Valerio <julio.valerio2@live.lagcc.cuny.edu>
2021-06-15 15:10:43 +0000

    Add Datastore Dependencies Create servlet for datastore library Create servlet for form creation Remove unused imports and commented code add textarea tags around text fields for user inputs

--- portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
@@ -1,28 +1,16 @@
 package com.google.sps.servlets;
-
 import com.google.gson.Gson;
-
-import org.eclipse.jetty.util.ajax.JSON;
-
 import java.io.IOException;
-import java.lang.reflect.Array;
-import java.util.List;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 
 /** Handles requests sent to the /hello URL. Try running a server and navigating to /hello! */
 @WebServlet("/hello")
 public class HelloWorldServlet extends HttpServlet {
 
   @Override
-//   public void doGet(final HttpServletRequest request, final HttpServletResponse response) throws IOException {
-//     response.setContentType("text/html;");
-//     response.getWriter().println("<h1>Hello Julio!</h1>");
-//   }
-
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     //Manually Create ArrayList String
     String ArrayList[] = new String[] {"Thanks", "for", "coming!"};
